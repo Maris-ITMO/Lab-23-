7f8df3eafaa1b7f02cf84997538950926d0677d4
Maris Shapiro <19maris72@gmail.com>
2023-02-10 23:47:46 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Lab_23_СамРабота_Асинхронное_программирование
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static async Task Main(string[] args)
+            {
+                Console.WriteLine("Максимальное значение в массиве равно: ");
+                int number = Convert.ToInt32(Console.ReadLine());
+                int result = await FactorialAsync(number);
+                Console.WriteLine($"Факториал из {number} is {result}.");
+                Console.ReadKey();
+            }
+
+            static async Task<int> FactorialAsync(int number)
+            {
+                int result = 1;
+                for (int i = 1; i <= number; i++)
+                {
+                    result *= i;
+                    await Task.Delay(100);
+                }
+                return result;
+            }
+        }
+    }
+}
